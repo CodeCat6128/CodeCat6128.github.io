82d55cf89f6759b7c2d72d2b5d2009dbbf6dbb75
Pavel <76407496+CodeCat6128@users.noreply.github.com>
2023-12-16 13:18:31 +0100

    ss

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'ravil.dsgn';
 export const SITE_DESCRIPTION = 'welcome :)';
 export const SITE_LOGO = '../public/site-logo.webp';
-export const SITE_FAVICON = '../public/';
+export const SITE_FAVICON = '../public/site-logo.webp';
