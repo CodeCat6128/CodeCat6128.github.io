0f9e25116ab1f929f37f5640a714c74263908a92
Pavel <76407496+CodeCat6128@users.noreply.github.com>
2023-12-16 12:38:47 +0100

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import mdx from '@astrojs/mdx';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: 'https://http://ravildsgnwebtestpage1612.github.io/',
+  base: '/src',
   integrations: [mdx(), tailwind()]
 });
