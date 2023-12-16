305bb952fd74c1c3f488152c4a48d294a67dd471
Pavel <76407496+CodeCat6128@users.noreply.github.com>
2023-12-16 13:27:55 +0100

    sssadwdsa

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,10 @@
 import rss from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
-export async function GET(context) {
+export async function GET() {
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
-		site: context.site,
+		site: 'https://codecat6128.github.io/',
 	});
 }
