75ed18bafb1d5633d44d0b27df5a3ccc1385dd6b
Pavel <76407496+CodeCat6128@users.noreply.github.com>
2023-12-16 13:34:04 +0100

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,16 +3,8 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET() {
 	return rss({
-	   title: SITE_TITLE,
-	   description: SITE_DESCRIPTION,
-	   site: 'https://codecat6128.github.io/',
-	   items: [
-		  {
-			 title: 'Заголовок записи',
-			 url: 'https://codecat6128.github.io/post-1',
-			 date: new Date(),
-			 description: 'Описание записи',
-		  },
-	   ],
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		site: 'https://codecat6128.github.io/'
 	});
 }
