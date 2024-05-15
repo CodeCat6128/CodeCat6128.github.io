4d6ab570feb54080be149c5fba74f344d311134c
Pavel <76407496+CodeCat6128@users.noreply.github.com>
2024-05-15 15:58:21 +0200

    rebase

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,17 +2,6 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		spacing: {
-			'1': '4px',
-			'2': '8px',
-			'3': '12px',
-			'4': '16px',
-			'5': '24px',
-			'6': '32px',
-			'7': '48px',
-			'8': '64px',
-			'9': '128px',
-		},
 		screens: {
 			'mc': '372px',
 			'sm': '452px',
@@ -22,14 +11,18 @@ export default {
 			'xl': '1280px',
 			'2xl': '1536px',
 		},
-		extend: {
-			boxShadow: {
-				's': '6px 6px 11px #d4d5d5, -6px -6px 11px #ffffff',
-				'm': '9px 9px 13px #d4d5d5, -9px -9px 13px #ffffff',
-				'l': '15px 15px 15px #d4d5d5, -15px -15px513px #ffffff',
-				'default': '0px 0px 90px #d4d5d5',
-				'bottom': '0px 45px 50px -45px #d4d5d5',
-			}
+		colors: {
+			'white': '#ffffff',
+			'black': '#000000',
+			'neutral': '#404040',
+			'error': '#ef4444',
+			'zinc': {
+				100: '#f4f4f5',
+				200: '#e4e4e7',
+				300: '#d4d4d8',
+				500: '#71717a',
+				800: '#27272a',
+			},
 		}
 	}
 }
