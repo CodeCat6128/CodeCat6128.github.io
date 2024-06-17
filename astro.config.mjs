5d5ca43c272cc8e311c0214ff4252243b002f7d7
Pavel Sheludko <sp6835317@gmail.com>
2024-06-17 18:32:04 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
-  adapter: vercel(),
+  integrations: [tailwind(), vercel()],
   site: 'https://codecat6128.github.io',
   base: 'CodeCat6128.github.io',
-})
+});
